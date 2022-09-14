b665d7378de164722574c83c2cefde17ac558bd7
ValerioGc <valerio.gc1@gmail.com>
2022-09-14 17:04:52 +0200

    Modifica chiamata API con chiamata Async

--- client_side/main.js
+++ client_side/main.js
@@ -9,7 +9,7 @@ var app = new Vue({
     methods: {
         async getResp (){
             try {
-                const response = await axios.get('//localhost/php-ajax-dischi/client_side/data_api.php')
+                const response = await axios.get('http://localhost/php-ajax-dischi/client_side/data_api.php')
                 this.albumData = response.data;
                 console.log(this.albumData);
             } catch(error) {
