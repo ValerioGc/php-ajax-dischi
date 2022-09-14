2d7af5087c88e872e813add3432e7fd09f9cf3a7
ValerioGc <valerio.gc1@gmail.com>
2022-09-14 15:54:13 +0200

    Fine Prima Versione

--- server_side/index.php
+++ server_side/index.php
@@ -1,14 +1,11 @@
 <?php
 
+
     require_once __DIR__ . "/../database.php";
 
-    var_dump($database);
-
     foreach ($database as $album) {
-//      var_dump($album);
         echo "<ul>";
         foreach ($album as $data) {
-//            var_dump($album);
             echo "<li>" . $data . "</li>";
         }
         echo "</ul>";
